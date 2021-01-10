7f59cb2a1a579146444a25211379aa7998cc68f3
soufiane121 <soufiane.oucherrou@gmail.com>
2021-01-10 15:01:58 -0500

    sidebar slide with animate hamburger

--- post-card-frontend/src/components/navBar.js
+++ post-card-frontend/src/components/navBar.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/navBar.scss";
 import { BiSearchAlt } from "react-icons/bi";
 
 const NavBar = () => {
+	const [open, setOpen] = useState(true);
+
+    //toggle open or close hamburger menu with animation
+	const handleHamburger = () => {
+		setOpen(!open);
+	};
 	return (
-		<nav className="nav-bar-container">
-			<span>Logo</span>
-			<ul className="nav-ul">
-				<li className="nav-li">Page 1</li>
-				<li className="nav-li">Page 2</li>
-				<li className="nav-li">Page 3</li>
-				<li className="nav-li">Page 4</li>
-			</ul>
-			<label className="login">Login</label>
-			{/* <div className="signup-cont"> */}
+		<>
+			<nav className="nav-bar-container">
+				<span className="logo">Logo</span>
+				<ul className={open ? "nav-ul hamburger" : "nav-ul active"}>
+					{" "}
+					<li className="nav-li">Page 1</li>
+					<li className="nav-li">Page 2</li>
+					<li className="nav-li">Page 3</li>
+					<li className="nav-li">Page 4</li>
+				</ul>
+
+				<label className="login">Login</label>
 				<span className="signup">SignUp </span>
-			{/* </div> */}
-			<BiSearchAlt className="icon" />
-		</nav>
+				<BiSearchAlt className="icon" />
+				<div className={open ? "hamburger" : "close"} onClick={handleHamburger}>
+					<span className="line border1"></span>
+					<span className="line border2"></span>
+					<span className="line border3"></span>
+				</div>
+			</nav>
+			{/* <ul className="nav-ul2">
+				<li className="nav-li2">Page 1</li>
+				<li className="nav-li2">Page 2</li>
+				<li className="nav-li2">Page 3</li>
+				<li className="nav-li2">Page 4</li>
+			</ul> */}
+		</>
 	);
 };
 

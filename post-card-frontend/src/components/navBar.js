cd2043cc396b889066c258bbe40fb4dcc45b6963
soufiane121 <soufiane.oucherrou@gmail.com>
2021-01-11 01:11:08 -0500

    fix overflow

--- post-card-frontend/src/components/navBar.js
+++ post-card-frontend/src/components/navBar.js
@@ -1,41 +1,61 @@
 import React, { useState } from "react";
 import "../styles/navBar.scss";
 import { BiSearchAlt } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import { AiOutlineClose } from "react-icons/ai";
 
 const NavBar = () => {
 	const [open, setOpen] = useState(true);
+	const [openSerch, setOpenSearch] = useState(false);
 
-    //toggle open or close hamburger menu with animation
+	//toggle open or close hamburger menu with animation
 	const handleHamburger = () => {
 		setOpen(!open);
 	};
+
+	//handle open input for search
+	const handleSearchInpt = () => {
+		setOpenSearch(!openSerch);
+	};
+
 	return (
 		<>
 			<nav className="nav-bar-container">
 				<span className="logo">Logo</span>
-				<ul className={open ? "nav-ul hamburger" : "nav-ul active"}>
-					{" "}
-					<li className="nav-li">Page 1</li>
-					<li className="nav-li">Page 2</li>
-					<li className="nav-li">Page 3</li>
-					<li className="nav-li">Page 4</li>
+				<ul className={open ? "nav-ul" : "nav-ul active"}>
+					<Link to="/main" className="nav-li">
+						<li>Home</li>
+					</Link>
+					<Link to="/main" className="nav-li">
+						<li>Home</li>
+					</Link>
+					<Link to="/main" className="nav-li">
+						<li>Home</li>
+					</Link>
+					<Link to="/main" className="nav-li">
+						<li>Home</li>
+					</Link>
 				</ul>
-
 				<label className="login">Login</label>
 				<span className="signup">SignUp </span>
-				<BiSearchAlt className="icon" />
+
+				<BiSearchAlt className="icon" onClick={handleSearchInpt} />
 				<div className={open ? "hamburger" : "close"} onClick={handleHamburger}>
 					<span className="line border1"></span>
 					<span className="line border2"></span>
 					<span className="line border3"></span>
 				</div>
 			</nav>
-			{/* <ul className="nav-ul2">
-				<li className="nav-li2">Page 1</li>
-				<li className="nav-li2">Page 2</li>
-				<li className="nav-li2">Page 3</li>
-				<li className="nav-li2">Page 4</li>
-			</ul> */}
+			<div className="search-cont">
+				<input
+					className={openSerch ? "inpt active" : "inpt"}
+					placeholder="Search cards"
+				/>
+				<AiOutlineClose
+					className={openSerch ? "close-icon active" : "close-icon"}
+					onClick={handleSearchInpt}
+				/>
+			</div>
 		</>
 	);
 };

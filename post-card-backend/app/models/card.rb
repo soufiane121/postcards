b4d08761e801ce9a330a3b919b097cf0ccd764d6
soufiane121 <soufiane.oucherrou@gmail.com>
2021-01-20 23:33:30 -0500

    scraping from website

--- post-card-backend/app/models/card.rb
+++ post-card-backend/app/models/card.rb
@@ -1,5 +1,17 @@
+require "rest-client"
+require "nokogiri"
+require "open-uri"
 class Card < ApplicationRecord
     has_many :gifts, dependent: :destroy
     has_many :users, through: :gifts
     validates :name, :category, :img, presence: true
+
+
+
+    def self.scrape_cards
+       answer = open("http://www.postable.com/cards")
+       doc = Nokogiri::HTML(answer)
+       p doc
+       byebug
+    end
 end
